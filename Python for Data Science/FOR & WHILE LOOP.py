eb8d330cf3ce2a12bf71d91fbd8c4f0c146be399
Rasky Wijaya <wjyrskyy14@gmail.com>
2026-02-12 22:35:23 +0700

    Add files via upload

---
+++ Python for Data Science/FOR & WHILE LOOP.py
@@ -0,0 +1,17 @@
+                        # foor loop
+
+for i in range(5):
+    print(i) # menampilkan i dari 0 sampai 4
+
+data = [10, 20, 30, 40]
+
+for i in data:
+    print(i) # menampilkan setiap elemen dalam list data
+
+                        # while loop
+
+# Looping berdasarkan kondisi
+i = 1
+while i <= 5:
+    print(i)
+    i += 1 #menambahkan nilai i sebanyak 1 setiap iterasi
