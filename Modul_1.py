7710deb2ee0e4976076d67d520ee55e88218fd49
Rasky Wijaya <wjyrskyy14@gmail.com>
2026-04-19 20:59:05 +0700

    Add files via upload

---
+++ Modul_1.py
@@ -0,0 +1,12 @@
+# MODUL = file .py yang berisi kode (fungsi, variabel, dll) yang bisa dipakai ulang
+        # Bayangin kamu punya banyak fungsi…
+        # Kalau semuanya ditaruh di 1 file → bakal berantakan 😵
+        #     Solusinya: pisahin ke file lain = itu disebut modul
+
+# contoh modul 1
+def tambah(a, b):
+    return a + b
+
+def kali(a,b):
+    return a * b
+
